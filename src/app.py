dbaee94f3884e9aa7ea3a2b6414751020f03fbf7
silvhua <silvhua@gmail.com>
2022-11-04 19:07:21 -0700

    correct transform_features function to retain only certain columns of df

---
+++ src/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import numpy as np
+import pickle
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+# Create a function to create log transformations of Total_Income and LoanAmount
+def transform_features(df):
+    """
+    - Returns a dataframe with:
+        - Log of the Total_Income
+        - Log of the LoanAmount
+        - Transformed features dropped
+    """
+    
+    # Log of loan amount
+    df['LoanAmount_log'] = np.log(df['LoanAmount'].astype('float64')) 
+
+    # Total income and log of total income
+    df['Total_Income'] = df['ApplicantIncome'] + df['CoapplicantIncome']
+    
+    transformed_feats = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Total_Income']
+    df['Total_Income_log'] = np.log(df['Total_Income'].astype('float64')) 
+
+    return df[['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed',
+       'Loan_Amount_Term', 'Credit_History', 'Property_Area', 'Total_Income_log','LoanAmount_log']]
+
+model = pickle.load(open('/Users/silvh/OneDrive/lighthouse/projects/mini-project-IV/model_random_forest-11-04.sav', "rb" ) )
+
+class predict(Resource):
+    def post(self):
+        json_data = request.get_json()
+        df = pd.DataFrame(json_data.values(), index=json_data.keys()).transpose()
+        # getting predictions from our model.
+        # it is much simpler because we used pipelines during development
+        res = model.predict(df)
+        # show the text
+        res = res.tolist()
+        res[0] = f'Model prediction: {res[0]}.'
+        if res == ['Y']:
+            res.append(dict({'Meaning of prediction': 'Loan application approval. ☺'}))
+        else: 
+            res.append(dict({'Meaning of prediction': 'Loan application denial. :( '}))
+
+        return res
+
+# assign endpoint
+api.add_resource(predict, '/predict')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
